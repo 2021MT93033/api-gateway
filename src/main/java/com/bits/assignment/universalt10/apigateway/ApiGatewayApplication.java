80713e57638f025cd27004ae8dc192dc26c975e6
2021MT93033 <balavignesh.premkumar@gmail.com>
2022-05-01 16:02:33 +0530

    Init setup

---
+++ src/main/java/com/bits/assignment/universalt10/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bits.assignment.universalt10.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
