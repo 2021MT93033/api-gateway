fa5b1d7ee1b6fe0503881c5c6ec65b0612e1478f
2021MT93033 <balavignesh.premkumar@gmail.com>
2022-11-19 20:25:56 +0530

    Gateway Service Changes containing Docker

--- src/main/java/com/bits/assignment/universalt10/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/bits/assignment/universalt10/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,11 @@
 package com.bits.assignment.universalt10.apigateway;
 
+import org.apache.logging.log4j.message.SimpleMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@GetMapping("/health")
+	public ResponseEntity<SimpleMessage> healthCheck() {
+		return ResponseEntity.ok(new SimpleMessage("The API Gateway is functioning"));
 	}
 
 }
